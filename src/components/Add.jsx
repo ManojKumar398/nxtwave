6a97cf68cccb7abc47a00dcf8f84aded9527539a
ManojKumar398 <manojkumar398626@gmail.com>
2022-11-05 11:40:47 +0530

    deploy website1

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {  Link, NavLink } from 'react-router-dom';
+import {   NavLink } from 'react-router-dom';
 import { VscChevronLeft } from "react-icons/vsc";
 import { Avatar, Button } from '@mui/material';
 import { useState } from 'react';
